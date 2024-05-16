481d8cb19fcec938067df42a47f63e97a620d84a
khaoularj <khaoula.raja1@gmail.com>
2024-05-16 03:51:06 -0700

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,31 @@
 """Write a function called filter_datum
 that returns the log message obfuscated"""
 import re
+from typing import List
+import logging
+import mysql.connector
+import os
 
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields, redaction, message, separator):
