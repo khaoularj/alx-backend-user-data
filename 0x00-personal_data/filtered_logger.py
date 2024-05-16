54f1f6739b797dcbbd7664a0fe40deefc2924a53
khaoularj <khaoula.raja1@gmail.com>
2024-05-16 04:15:09 -0700

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,79 +1,90 @@
 #!/usr/bin/env python3
-"""
-    RedactingFormatter
-"""
-import csv
+"""Write a function called filter_datum
+that returns the log message obfuscated"""
+import re
 import logging
 import os
-import re
 from typing import List
 import mysql.connector
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ init """
+        """ Initialize the formatter with the fields to redact"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ format record """
+        """ Format the log record with redacted PII data"""
         return filter_datum(self.fields, self.REDACTION,
                             super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ returns the log message obfuscated """
-    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
-                  lambda match: match.group(1) + "=" + redaction
-                  if match.group(1) in fields else match.group(0), message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """functions that returns the log message obfuscated"""
+    pattern = r"({})=[^{}]*".format(
+        "|".join(map(re.escape, fields)),
+        re.escape(separator))
+    return re.sub(
+        pattern,
+        lambda match: "{}={}".format(match.group(1), redaction),
+        message)
 
 
 def get_logger() -> logging.Logger:
-    """ return a logger object """
-    lg = logging.getLogger("user_data")
-    lg.setLevel(logging.INFO)
-    lg.propagate = False
-    sh = logging.StreamHandler()
-    sh.setFormatter(RedactingFormatter(PII_FIELDS))
-    lg.addHandler(sh)
-    return lg
+    """ return a logger with a RedactingFormatter """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ connect to MySQL database """
+    """ Connect to MySQL database """
     return mysql.connector.connect(
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "root"),
+        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
         database=os.getenv("PERSONAL_DATA_DB_NAME"),
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "localhost"),
+        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
     )
 
 
 def main():
-    """
-    main function
-    """
-    con = get_db()
-    users = con.cursor()
-    users.execute("SELECT CONCAT('name=', name, ';ssn=', ssn, ';ip=', ip, \
-        ';user_agent', user_agent, ';') AS message FROM users;")
-    formatter = RedactingFormatter(fields=PII_FIELDS)
+    """ the main function to fetch data"""
+    db_conn = get_db()
+    cursor = db_conn.cursor()
+
+    query = (
+        "SELECT CONCAT("
+        "'name=', name, ';ssn=', ssn, ';ip=', ip, "
+        "';user_agent=', user_agent, ';'"
+        ") AS message "
+        "FROM users;"
+    )
+    cursor.execute(query)
+
     logger = get_logger()
 
-    for user in users:
-        logger.log(logging.INFO, user[0])
+    for row in cursor:
+        logger.info(row[0])
+
+    cursor.close()
+    db_conn.close()
 
 
 if __name__ == "__main__":
