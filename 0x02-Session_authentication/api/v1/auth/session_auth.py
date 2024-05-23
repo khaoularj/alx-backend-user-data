bb390439da3118ca6a0f00791f14d8373a66e6f2
khaoularj <khaoula.raja1@gmail.com>
2024-05-23 10:54:16 -0700

    adding a new authentication mechanism

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" class SessionAuth that inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """this class inherits from Auth class"""
+    pass
