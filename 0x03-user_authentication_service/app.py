c81978a7def283b62b1cb5c65ed93cb17b63367b
khaoularj <khaoula.raja1@gmail.com>
2024-05-30 03:35:10 -0700

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -38,5 +38,17 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """function that handle the logout"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
